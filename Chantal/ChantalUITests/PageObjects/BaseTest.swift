43e8d75ad859d5f131e9bd103bd2db5037f9df5e
d.zjumalin <d.zjumalin@cft.ru>
2025-05-20 20:09:03 +0700

    Added func for type elements

--- Chantal/ChantalUITests/PageObjects/BaseTest.swift
+++ Chantal/ChantalUITests/PageObjects/BaseTest.swift
@@ -10,7 +10,7 @@ import XCTest
 
 class Logger {
 	func log(_ mlog: String) {
-		NSlog(mlog)
+		NSLog(mlog)
 	}
 }
 
@@ -19,8 +19,44 @@ public class BaseTest {
 	let app = XCUIApplication()
 	let log = Logger().log
 	required init(timeout: TimeInterval = 10, completion: Completion = nil) {
+		log("waiting \(timeout)s for \(String(describing: self)) existence")
 		XCTAssert(rootElement.waitForExistence(timeout: timeout),
 				  "Page \(String(describing: self)) waited, but not loaded")
-		completion
+		completion?()
 	}
+
+	var rootElement: XCUIElement {
+		fatalError("subclass should override rootElement")
+	}
+
+	// Button
+	func button(_ name: String) -> XCUIElement {
+		return app.buttons[name]
+	}
+
+	// Navigation Bar
+	func navBar(_ name: String) -> XCUIElement {
+		return app.navigationBars[name]
+	}
+
+	// SecureField
+	func secureField(_ name: String) -> XCUIElement {
+		return app.secureTextFields[name]
+	}
+
+	// TextField
+	func textField(_ name: String) -> XCUIElement {
+		return app.textFields[name]
+	}
+
+	// TextView
+	func textView(_ name: String) -> XCUIElement {
+		return app.textViews[name]
+	}
+
+	// Text
+	func text(_ name: String) -> XCUIElement {
+		return app.staticTexts[name]
+	}
+
 }
